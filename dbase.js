0711543a6c6d7a74556c45a6112edcb2eda5ce99
iton7 <71405914+iton7@users.noreply.github.com>
2020-09-22 21:29:24 +0300

    Add files via upload

---
+++ dbase.js
@@ -0,0 +1,373 @@
+     const data1 = [ ///////////////////////////////////////////////////////////////////////////////////////// ЛЮДИ
+     {
+      image: 'people/batum_mesttn_guriitsi.png',
+     title: 'Кавказские типы : Гурийцы .',
+     description: 'Фото начала XX века' 
+     },{     
+      image: 'people/adjarets.png',
+     title: 'Аджарец. Худ. М.Тильке . 1910 г.',
+     description: 'Почтовая открытка. 1936 г.' 
+     },{  
+        image: 'people/lazi_gornii_ziteli_batuma.png',
+     title: 'Лазы, горные жители Батума .',
+     description: 'Почтовая открытка начала 20 века.' 
+     },{        
+        image: 'people/lazi_800.png',
+     title: 'Лазы, горные жители Батума . № 2',
+     description: 'Фотография начала 20 века.' 
+     },{       
+      image: 'people/prokudin_okrestn_batuma_kurdianka.jpg',
+     title: 'Курдская женщина с детьми. Близ сел. Кварцхана.',
+     description: 'Сел. Кварцхана находится по пути из Батума в Артвин. Фото С.М. Прокудин-Горский между 1905 и 1915 гг.' 
+     },{         
+      image: 'people/prokudin_okrestn_batuma_armianka.jpg',
+     title: 'Артвин. Армянка (католичка) в национальном костюме. ',
+     description: 'Фото С.М. Прокудин-Горский между 1905 и 1915 гг.'  
+    //   datasrc: "некоторый текст 2"       
+      },{         
+      image: 'people/prokudin_artvin_armyanki_v_obich_costume.jpg',
+     title: 'Артвин. Армянки (католички) в обычном костюме. ',
+     description: 'Фото С.М. Прокудин-Горский между 1905 и 1915 гг.'     
+      },{
+     //thumb: 'images/Окрестности Батума. Чаква. Группа рабочих на сборе чая. Гречанки.jpg',
+     image: 'people/okrestnosti_batuma_chakva_grechanki.jpg',
+     title: 'Окр. Батума. Чаква. Группа рабочих на сборе чая. Гречанки',
+     description: 'Фото С.М. Прокудин-Горский между 1905 и 1915 гг.'
+     },{
+     image: 'people/mulli_v_mecheti_asisie_1912.jpg',
+     title: 'Муллы в мечети Азизия. 1912',
+     description: 'Фото С.М. Прокудин-Горский между 1905 и 1915 гг.'
+     },{
+     //thumb: 'images/batum_kurdianka.jpg',
+     image: 'people/batum_kurdianka.jpg',
+     title: 'Батум. Курдианка',
+     description: 'Фото начала XX века'
+     },{
+     //thumb: 'images/batum_kurdiankш.jpg',
+     image: 'people/batum_kurdianki.jpg',
+     title: 'Батум. Курдианки',
+     description: 'Фото начала XX века'
+     },{  
+     image: 'okrestnosti/okrestn_batuma_vspashka.jpg',
+     title: 'Окрестности Батума. Вспашка местной сохой .',
+     description: 'Фото начала XX века'
+     },{  
+     image: 'okrestnosti/okestn_batuma_kochevniki_kurdi.jpg',
+     title: 'Окрестности Батума. Временная стоянка кочевников курдов .',
+     description: 'Фото начала XX века'  
+     },{   
+     image: 'people/adjarets.jpg',
+     title: 'Типы Кавказа. Аджарец.',
+     description: 'Фото начала XX века'
+     },{
+     image: 'people/okrestnosti_batuma_turetskaya_skola.jpg',
+     title: 'Окрестности Батума. Турецкая школа.',
+     description: 'Фото начала XX века'
+     },{
+     image: 'people/kavkazskie_tipi_musha.jpg',
+     title: 'Кавказские типы. Муша .',
+     description: 'Фото начала XX века'
+     },{
+     //thumb: 'images/Батум. Группа местных жителей.jpg',
+     image: 'people/batum_gruppa_mestn_zitelei.jpg',
+     title: 'Батум. Группа местных жителей',
+     description: 'Фото начала XX века'
+     },{
+     image: 'people/batum_mestn_tip_rabochego_musha.jpg',
+     title: 'Батум местный тип рабочего \"муша\"',
+     description: 'Фото начала XX века'
+     },{
+     image: 'people/batum_prodavets_cholodnoi_vodi.jpg',
+     title: 'Батум. Продавец холодной воды',
+     description: 'Фото начала XX века'
+
+     }];
+
+
+     const data2 = [ ////////////////////////////////////////////////////////////////////////////культ
+     {
+     image: 'people/mulli_v_mecheti_asisie_1912.jpg',
+     title: 'Муллы в мечети Азизия. 1912',
+     description: 'Фото С.М. Прокудин-Горский между 1905 и 1915 гг.'
+     },{
+     image: 'kult/batum_mechet_azizie_1.jpg',
+     title: 'Батум.Мечеть Азизие.',
+     description:  'Фото начала XX века'
+     },{
+     image: 'kult/batum_mechet_azizie_2.jpg',
+     title: 'Батум.Мечеть Азизие.',
+     description:  'Фото начала XX века'
+     },{
+     image: 'kult/batum_armyansk_tserkov.jpg',
+     title: 'Батум. Армянская церковь',
+     description:  'Фото начала XX века'
+      },{
+     image: 'kult/batum_kostel.png',
+     title: 'Батум. Католическая церковь .',
+     description:  'Фото начала XX века'    
+      },{
+     image: 'kult/batum_kostel_15.png',
+     title: 'Батум. Католическая церковь . # 2',
+     description:  'Фото начала XX века'       
+      },{
+     image: 'kult/sobor.png',
+     title: 'Батум. Собор .',
+     description:  'Фрагмент фото С.М.Прокудина-Горского .'           
+       },{
+     image: 'kult/batum_zapadn_chast.png',
+     title: 'Батум. Западная часть .',
+     description:  'Фото начала XX века'        
+     }];
+
+     const data3 = [ ////////////////////////////////////////////////////////////////////////////УЛИЦЫ
+     {
+     image: 'streets/batum_mar_prosp.jpg',
+     title: 'Батум. Мариинский проспект .',
+     description: 'Фото начала XX века'
+     },{ 
+     image: 'streets/batum_vokzal_46.png',
+     title: 'Батум . Вокзал.',
+     description:  'Фото начала XX века'
+      },{ 
+     image: 'streets/batum_mariinski_winter.jpg',
+     title: 'Батум. Мариинский проспект. Зима 1911 г.',
+     description:  'Фото начала XX века'  
+        },{ 
+     image: 'streets/batum_tiflisskaya_ulitsa_1.png',
+     title: 'Батум. Тифлисская улица . №1',
+     description:  'Фото начала XX века'   
+         },{ 
+     image: 'streets/batum_tiflisskaya_ulitsa_2.jpg',
+     title: 'Батум. Тифлисская улица . №2',
+     description:  'Фото начала XX века' 
+      },{ 
+     image: 'streets/batum_pochta.png',
+     title: 'Батум. Почтово - телеграфная контора .',
+     description:  'Фото начала XX века'              
+     }];
+
+     const data4 = [ ////////////////////////////////////////////////////////////////////////////port
+     {
+     image: 'port/batumskaya_buchta.jpg',
+     title: 'Батумская бухта.',
+     description:  'Фото С.М. Прокудин-Горский между 1905 и 1915 гг.'  
+     },{      
+     image: 'port/batum_buhta_congr.jpg',
+     title: 'Батум . Бухта.',
+     description:  'между 1910 и 1915 гг. src: Libr. of US Congress'  
+     },{   
+     image: 'port/batum_mayak.jpg',
+     title: 'Батум . Маяк.',
+     description:  'Фото начала XX века'
+     },{
+     image: 'port/batum_buhta.jpg',
+     title: 'Батум . Бухта',
+     description:  'Фото начала XX века'   
+      },{
+     image: 'port/batum_lagorio.jpg',
+     title: 'Батум. Порт. Картина 1881 г. Лев Лагорио .',
+     description:  'Картина 19 века'       
+      },{
+     image: 'port/batum_port.jpg',
+     title: 'Батум. Порт.',
+     description:  'Фото И.М. Кахиани'       
+     }];
+
+     const data5 = [ ////////////////////////////////////////////////////////////////////////////pioneer
+     {
+     image: 'park/batum_nur_gel.jpg',
+     title: 'Батум . Озеро Нурие-Гель.',
+     description:  'Фото начала XX века'
+     },{
+     image: 'park/nurie_gel.jpg',
+     title: 'Пионерский парк',
+     description: 'Фото С.Фридлянд , 70-80 гг.'
+     },{
+     image: 'park/batum_gorsad_i_ozero.jpg',
+     title: 'Батум . Городской сад и озеро',
+     description:  'Фото начала XX века'
+     }];
+
+     const data6 = [ ////////////////////////////////////////////////////////////////////////////bulvar
+     {
+     image: 'bulvar/batum_na_beregu_morya.jpg',
+     title: 'Батум . На берегу моря.',
+     description:  'Фото начала XX века'
+     },{
+     image: 'bulvar/batum_bulvar_alleya_k_moru.jpg',
+     title: 'Батум . Бульвар . Аллея к морю .',
+     description:  'Фото начала XX века' 
+          },{
+     image: 'bulvar/batum_nabereznaya.jpg',
+     title: 'Батум .Набережная .',
+     description:  'Фото начала XX века' 
+     }];
+
+
+     const data7 = [ ////////////////////////////////////////////////////////////////////////////окрестности
+     {
+     image: 'okrestnosti/batum_chorokh.png',
+     title: 'Река Чорох близ Батума.',
+     description: 'Фото начала XX века'
+     },{  
+     image: 'okrestnosti/vid_na_batumskoe_poberezhe.jpg',
+     title: 'Окрестности Батума. Чаква. Вид на Батумское побережье .',
+     description: 'Фото С.М. Прокудин-Горский между 1905 и 1915 гг.'
+     },{  
+     image: 'okrestnosti/okrestn_batuma_vspashka.jpg',
+     title: 'Окрестности Батума. Вспашка местной сохой .',
+     description: 'Фото начала XX века'
+     },{  
+     image: 'okrestnosti/okestn_batuma_kochevniki_kurdi.jpg',
+     title: 'Окрестности Батума. Временная стоянка кочевников курдов .',
+     description: 'Фото начала XX века'  
+     },{  
+     image: 'okrestnosti/okres_batuma_st_kobuleti.jpg',
+     title: 'Окрестности Батума. Станция Кобулети .',
+     description: 'Image 1 description'
+     },{
+     image: 'prokudin/okr_batuma_chakva.jpg',
+     title: 'Окрестности Батума. Чаква. Общий вид чайной фабрики .',
+     description: 'Фото С.М. Прокудин-Горский между 1905 и 1915 гг.'
+      },{
+     image: 'artvin/artvinskoe_sosse.jpg',
+     title: 'Окрестности Батума . Артвинское шоссе .',
+     description:  'Фото начала XX века'
+     },{
+     image: 'artvin/okestn_batuma_artvinsk_sosse.jpg',
+     title: 'Окрестности Батума . Артвинское шоссе . #2',
+     description:  'Фото начала XX века'
+     },{   
+     image: 'okrestnosti/okrestnosti_batuma_tsichisdziri.jpg',
+     title: 'Окрестности Батума . Цихисдзири .',
+     description:  'Фото начала XX века'  
+      },{   
+     image: 'okrestnosti/okrestn_batuma_tsikhisdziri_orlinoye_gnezdo.png',
+     title: 'Окрестности Батума . Цихисдзири . Орлиное гнездо .',
+     description:  'Фото начала XX века'      
+      },{   
+     image: 'okrestnosti/okestn_batuma_zel_mis__alleya_redk_palm.jpg',
+     title: 'Окрестности Батума .Зеленый Мыс. Аллея редких пальм .',
+     description:  'Фото начала XX века'    
+       },{   
+     image: 'okrestnosti/okrestn_batuma_mectechko_erge.jpg',
+     title: 'Окрестности Батума . Местечко Эрге .',
+     description:  'Фото начала XX века'       
+      }];
+
+     const data8 = [ ////////////////////////////////////////////////////////////////////////////batumi_USSR
+      {
+     image: 'batumi_ussr/batoumi_restoran_salkhino.jpg',
+     title: 'Батуми. Ресторан \" Салхино \"  на бульваре .  ',
+     description: 'Фото  70-80 гг.'
+      },{
+     image: 'park/nurie_gel.jpg',
+     title: 'Пионерский парк',
+     description: 'Фото С.Фридлянд , 70-80 гг.'
+      },{
+     image: 'batumi_ussr/batumi_kino_tbilisi.jpg',
+     title: 'Батуми. Кинотеатр \" Тбилиси \"',
+     description: 'Фото  70-80 гг.'    
+     }];
+     const data9 = [ ////////////////////////////////////////////////////////////////////////////prokudin
+     {
+     image: 'prokudin/obshii_vid_s_vostoka.png',
+     title: 'Батум. Общий вид с востока . Виден собор.',
+     description: 'Фото С.М. Прокудин-Горский между 1905 и 1915 гг.'
+     },{  
+     image: 'prokudin/batum_obshchii_vid_s_bukhtoi.png',
+     title: 'Батум. Общий вид с бухтой .',
+     description: 'Фото С.М. Прокудин-Горский между 1905 и 1915 гг.'
+     },{   
+     image: 'prokudin/okr_batuma_chakva.jpg',
+     title: 'Окрестности Батума. Чаква. Общий вид чайной фабрики .',
+     description: 'Фото С.М. Прокудин-Горский между 1905 и 1915 гг.'
+     },{
+     image: 'people/mulli_v_mecheti_asisie_1912.jpg',
+     title: 'Муллы в мечети Азизия. 1912',
+     description: 'Фото С.М. Прокудин-Горский между 1905 и 1915 гг.'
+     },{
+     image: 'people/prokudin_okrestn_batuma_kurdianka.jpg',
+     title: 'Курдская женщина с детьми. Близ сел. Кварцхана.',
+     description: 'Сел. Кварцхана находится по пути из Батума в Артвин. Фото С.М. Прокудин-Горский между 1905 и 1915 гг.'  
+      },{   
+     image: 'artvin/artvin.jpg',
+     title: 'Артвин. Общий вид .',
+     description: 'Фото С.М. Прокудин-Горский между 1905 и 1915 гг.'   
+     },{  
+      image: 'people/prokudin_okrestn_batuma_armianka.jpg',
+     title: 'Артвин. Армянка (католичка) в национальном костюме. ',
+     description: 'Фото С.М. Прокудин-Горский между 1905 и 1915 гг.'  
+      },{ 
+     image: 'people/prokudin_artvin_armyanki_v_obich_costume.jpg',
+     title: 'Артвин. Армянки (католички) в обычном костюме. ',
+     description: 'Фото С.М. Прокудин-Горский между 1905 и 1915 гг.'
+     },{  
+     image: 'people/okrestnosti_batuma_chakva_grechanki.jpg',
+     title: 'Окр. Батума. Чаква. Группа рабочих на сборе чая. Гречанки',
+     description: 'Фото С.М. Прокудин-Горский между 1905 и 1915 гг.'
+     },{
+     image: 'okrestnosti/vid_na_batumskoe_poberezhe.jpg',
+     title: 'Окрестности Батума. Чаква. Вид на Батумское побережье',
+     description: 'Фото С.М. Прокудин-Горский между 1905 и 1915 гг.'  
+     },{  
+     image: 'port/batumskaya_buchta.jpg',
+     title: 'Батумская бухта.',
+     description:  'Фото С.М. Прокудин-Горский между 1905 и 1915 гг.'     
+     }];
+      const data10 = [ ////////////////////////////////////////////////////////////////////////////artvin
+     {
+     image: 'artvin/artvin.jpg',
+     title: 'Артвин. Общий вид .',
+     description: 'Фото С.М. Прокудин-Горский между 1905 и 1915 гг.'  
+      },{ 
+     image: 'artvin/artvin_II.png',
+     title: 'Улицы в Артвине. ',
+     description: 'Фото С.М. Прокудин-Горский между 1905 и 1915 гг.'  
+      },{ 
+       image: 'people/prokudin_okrestn_batuma_kurdianka.jpg',
+     title: 'Курдская женщина с детьми. Близ сел. Кварцхана.',
+     description: 'Сел. Кварцхана находится по пути из Батума в Артвин. Фото С.М. Прокудин-Горский между 1905 и 1915 гг.'  
+      },{         
+      image: 'people/prokudin_okrestn_batuma_armianka.jpg',
+     title: 'Артвин. Армянка (католичка) в национальном костюме. ',
+     description: 'Фото С.М. Прокудин-Горский между 1905 и 1915 гг.'  
+      },{ 
+     image: 'artvin/artvinskoe_sosse.jpg',
+     title: 'Окрестности Батума . Артвинское шоссе .',
+     description:  'Фото начала XX века'
+     },{
+     image: 'artvin/okestn_batuma_artvinsk_sosse.jpg',
+     title: 'Окрестности Батума . Артвинское шоссе . #2',
+     description:  'Фото начала XX века'
+      },{ 
+     image: 'artvin/okr_batuma_artvinsk_kayuk.jpg',
+     title: 'Окрестности Батума . Артвинский каяк на Чорохе .',
+     description:  'Фото начала XX века'  
+     },{   
+     image: 'artvin/turkey_stamps.png',
+     title: 'Почт. марки Турецкой Республики про Артвин',
+     description: 'Почт. марки разных лет'  
+     }];
+
+     
+/////////////////////////////////////////////////////////////////////////////////////////////
+function removeDuplicates(originalArray, prop) {
+    //removes duplicates from array of objects in field prop
+    
+     var newArray = [];
+     var lookupObject  = {};
+
+     for(var i in originalArray) {
+        lookupObject[originalArray[i][prop]] = originalArray[i];
+     }
+
+     for(i in lookupObject) {
+         newArray.push(lookupObject[i]);
+     }
+      return newArray;
+ }
+/////////////////////////////////////////////////////////////////////////////////////////////
+data_with_duplicates =data1.concat(data2,data3,data4,data5,data6,data7,data8,data9,data10);
+
+const data11=removeDuplicates(data_with_duplicates, "image");
